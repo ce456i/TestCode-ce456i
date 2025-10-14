4735d9ac2cc2897b3ec2e6dc4152f670cac83cb6
ce456i <elvermark@qq.com>
2025-10-14 12:22:42 +0800

    Add files via upload

---
+++ Cdemo/三角函数sin值.cpp
@@ -0,0 +1,16 @@
+#include<math.h>
+#include<stdio.h>
+
+int main() {
+	int n;
+	double t;
+	scanf_s("%d", &n);
+	if (n > 360) {
+		printf("?\n");
+	}
+	else{
+		t = sin(n);
+		printf("%f\n", t);
+	}
+	return 0;
+}

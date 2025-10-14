d3394a90f674305e6fce0066398bc550cd87d4eb
ce456i <elvermark@qq.com>
2025-10-14 12:21:17 +0800

    Add files via upload

---
+++ Cdemo/闰年判断.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main() {
+	int y;
+	scanf_s("%d", &y);
+	if ((y % 4 == 0 && y % 100 != 0) || (y % 400 == 0)) {
+		printf("yes\n");
+	}
+	else {
+		printf("no\n");
+	}
+	return 0;
+}
